a03900542d18c5d8617a28facb7b675bfae59507
Alex O'Donovan <email.alexodonovan@gmail.com>
2018-03-14 22:48:06 +0000

    Basic conversation dialog, vscode debug launch config

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var builder = require('botbuilder');
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat connector for communicating with the Bot Framework Service
@@ -17,6 +17,12 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-});
+var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        session.send('Every great business started with an idea');
+        return session.beginDialog('questions');
+    }
+]);
+
+bot.dialog('questions', require('./questions'));
+// bot.dialog('')
