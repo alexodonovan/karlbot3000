9c614afe466d75e8a5e1841f6be92c8959bb9a3a
Alex O'Donovan <email.alexodonovan@gmail.com>
2018-03-22 22:46:13 +0000

    Show hero card as first message

--- app.js
+++ app.js
@@ -20,9 +20,29 @@ server.post('/api/messages', connector.listen());
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, [
     function (session) {
-        session.send('Every great business started with an idea');
-        return session.beginDialog('market');
+        if (session.message.text === 'start_dialog') {
+            return session.beginDialog('market');
+        }
+
+        var card = createHeroCard(session);
+        // attach the card to the reply message
+        var msg = new builder.Message(session).addAttachment(card);
+        session.send(msg);
+
     }
 ]);
+
+function createHeroCard(session) {
+    return new builder.HeroCard(session)
+        .title('Innovation Questionnaire')
+        .subtitle('Every great business started with an idea')
+        .text('To help better understand the potential of your idea, we\'ve developed a simple questionnaire.')
+        .images([
+            builder.CardImage.create(session, 'https://fexco.com/wp-content/uploads/2018/03/fexco-300x300.jpg')
+        ])
+        .buttons([
+            builder.CardAction.postBack(session, 'start_dialog', 'Get Started')
+        ]);
+}
 
 bot.dialog('market', questions.market);
